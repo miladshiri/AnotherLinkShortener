36de04285c80cc8b7e7853086bf7f7ff8ce4dedf
Milad Shiri <miladonlinemail@gmail.com>
2022-06-19 08:26:30 +0300

    short link and redirect

--- linkshorter/core/urls.py
+++ linkshorter/core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('makeshortlink', views.linkshorter, name='makeshortlink'),
+    path("", views.home, name='home'),
+    path("makeshortlink", views.make_shortlink, name='make_shortlink'),
+    path("<slug:slug>", views.goto_shortlink, name='goto_shortlink')
 ]
--- linkshorter/core/views.py
+++ linkshorter/core/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
+from django.urls import reverse
+from django.utils.crypto import get_random_string
+from .models import Link
 
 
-def home():
-    pass
+@require_http_methods(["GET"])
+def home(request):
+    return render(request, 'core/home.html', {})
 
-def linkshorter():
-    pass
+@require_http_methods(["POST"])
+def make_shortlink(request):
+    shorted_url = get_random_string(5)
+    original_url = request.POST['original_url']
+
+    new_link = Link(original_url=original_url, shorted_url=shorted_url)
+    new_link.save()
+    return JsonResponse({"success":True, "shorted_link":reverse('goto_shortlink', args=[shorted_url])}, status=200)
+
+
+@require_http_methods(["GET"])
+def goto_shortlink(request, slug):
+    link = Link.objects.get(shorted_url=slug)
+    return redirect(link.original_url)
